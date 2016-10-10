2017f9efc0a748d2c8cf6b1e6cc4a24ac009315c
Bert Peters <bert.ljpeters@gmail.com>
2016-10-10 14:11:05 +0200

    Allow user creation from script.

--- src/commands/CreateUserCommand.php
+++ src/commands/CreateUserCommand.php
@@ -16,6 +16,18 @@ class CreateUserCommand extends GatekeeperCommand
                 "optional" => true,
                 "description" => "Do not activate the user by default.",
             ],
+            "username" => [
+                "optional" => true,
+                "description" => "username to create. Will prompt if not present.",
+            ],
+            "email" => [
+                "optional" => true,
+                "description" => "email to use. Will prompt if not present.",
+            ],
+            "password" => [
+                "optional" => true,
+                "description" => "password to use. Will prompt if not present.",
+            ],
         ],
     ];
 
@@ -24,21 +36,28 @@ class CreateUserCommand extends GatekeeperCommand
      *
      * @param boolean|string $noactivate
      */
-    public function execute($noactivate = false)
+    public function execute($noactivate = false, $password = null, $username = null, $email = null)
     {
-        $username = $this->question("Username");
-        $email = $this->question("Email");
-        $password = $this->secret("Password", null, false);
+        if ($username == null) {
+            $username = $this->question("Username");
+        }
+        if ($email == null) {
+            $email = $this->question("Email");
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                $this->error("Invalid email supplied.");
+                return;
+            }
+        }
+        if ($password == null) {
+            $password = $this->secret("Password", null, false);
 
-        if ($password != $this->secret("Repeat", null, true)) {
-            $this->error("Passwords do not match.");
-            return;
+            if ($password != $this->secret("Repeat", null, true)) {
+                $this->error("Passwords do not match.");
+                return;
+            }
         }
 
-        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            $this->error("Invalid email supplied.");
-            return;
-        }
+
 
         $user = $this->gatekeeper->createUser($email, $username, $password, !$noactivate);
 
